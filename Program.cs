54a98b04b5dea61d99ce642633bcfdcbbed228ed
Frostrion <sastoinev@gmail.com>
2020-12-26 16:14:26 +0200

    Thrid commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace GrpProject
 {
@@ -7,6 +7,7 @@ namespace GrpProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!!");
         }
     }
 }
